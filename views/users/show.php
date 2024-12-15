228a7d3add5f66a67d3ad98899cabe0369213a23
Alex Cobas <alexcobasfp@ibf.cat>
2024-12-15 12:48:11 +0100

    show commit

--- views/users/show.php
+++ views/users/show.php
@@ -2,8 +2,8 @@
     <section class="fixed-top bg-white">
         <nav class="navbar navbar-expand-lg container mt-0">
             <div class="container-fluid">
-                <a class="navbar-brand" href="<?=url?>home/">
-                    <img src="<?=url?>img/logo.webp" class="logoSuperior" alt="Logo">
+                <a class="navbar-brand" href="<?= url ?>home/">
+                    <img src="<?= url ?>img/logo.webp" class="logoSuperior" alt="Logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -21,22 +21,26 @@
                                 <i class="bi bi-person-circle icon-24"></i>
                             </div>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="customDropdown" style="right: 0; left: auto; transform-origin: right top;">
-                            <?php if(empty($_SESSION["user"])){?>
-                                <li><a class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#loginModal">Iniciar sesión</a></li>
-                                <li><a class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#registerModal">Regístrate</a></li>
-                            <?php } else{?>
-                                <li><a class="dropdown-item" href="<?=url?>user/">Cuenta</a></li>
-                                <li><a class="dropdown-item" href="<?=url?>user/order">Ver mis pedidos</a></li>
-                            <?php }?>
-                                <li><hr class="dropdown-divider"></li>
+                                <?php if (empty($_SESSION["user"])) { ?>
+                                    <li><a class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#loginModal">Iniciar sesión</a></li>
+                                    <li><a class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#registerModal">Regístrate</a></li>
+                                <?php } else { ?>
+                                    <li><a class="dropdown-item" href="<?= url ?>user/">Cuenta</a></li>
+                                    <li><a class="dropdown-item" href="<?= url ?>user/order">Ver mis pedidos</a></li>
+                                <?php } ?>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
                                 <li><a class="dropdown-item" href="#">Tarjetas regalo</a></li>
                                 <li><a class="dropdown-item" href="#">Pon tu casa en Airbnb</a></li>
                                 <li><a class="dropdown-item" href="#">Ofrece una experiencia</a></li>
                                 <li><a class="dropdown-item" href="#">Centro de ayuda</a></li>
-                            <?php if(!empty($_SESSION["user"])){?>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="<?=url?>user/logout">Cerrar sesion</a></li>
-                            <?php }?>
+                                <?php if (!empty($_SESSION["user"])) { ?>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="<?= url ?>user/logout">Cerrar sesion</a></li>
+                                <?php } ?>
                             </ul>
                         </div>
                     </div>
@@ -51,7 +55,7 @@
             </div>
             <div class="offcanvas-body">
                 <?php if (!empty($products)) { ?>
-                <?php $totalPrice=0?>
+                    <?php $totalPrice = 0 ?>
                     <?php foreach ($products as $product) { ?>
                         <div class="product-item border-bottom pb-2">
                             <div class="d-flex justify-content-between align-items-center">
@@ -67,7 +71,7 @@
                             </div>
                         </div>
                     <?php
-                    $totalPrice += $product->getBase_Price();
+                        $totalPrice += $product->getBase_Price();
                     } ?>
                 <?php } else { ?>
                     <p>No hay productos en el carrito.</p>
@@ -76,7 +80,7 @@
             <div class="offcanvas-footer border-top p-3">
                 <div class="d-flex justify-content-between">
                     <p class="fw-bold mb-0">Total:</p>
-                    <p class="fw-bold mb-0"><?=number_format($totalPrice, 2)?> €</p>
+                    <p class="fw-bold mb-0"><?= number_format($totalPrice, 2) ?> €</p>
                 </div>
                 <button type="button" class="btn btn-primary w-100 mt-2">Ir al carrito</button>
             </div>
@@ -108,9 +112,9 @@
                             <a href="#" class="text-decoration-none">¿Olvidaste tu contraseña?</a>
                         </div>
                         <input type="hidden" value="product" name="controller">
-                        <?php if(isset($_GET["errorLogin"])){?>
-                            <p class="errorMessage"><?=$_GET["errorLogin"]?></p>
-                        <?php }?>
+                        <?php if (isset($_GET["errorLogin"])) { ?>
+                            <p class="errorMessage"><?= $_GET["errorLogin"] ?></p>
+                        <?php } ?>
                         <button type="submit" class="btn btn-primary w-100 mt-3">Iniciar sesión</button>
                     </form>
                     <p class="text-center mt-3">¿No tienes cuenta? <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal" data-bs-dismiss="modal">Regístrate aquí</a></p>
@@ -152,9 +156,9 @@
                             <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
                         </div>
                         <input type="hidden" value="product" name="controller">
-                        <?php if(isset($_GET["errorRegister"])){?>
-                            <p class="errorMessage"><?=$_GET["errorRegister"]?></p>
-                        <?php }?>
+                        <?php if (isset($_GET["errorRegister"])) { ?>
+                            <p class="errorMessage"><?= $_GET["errorRegister"] ?></p>
+                        <?php } ?>
                         <button type="submit" class="btn btn-primary w-100 mt-3">Crear cuenta</button>
                     </form>
                     <p class="text-center mt-3">¿Ya tienes una cuenta? <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal" data-bs-dismiss="modal">Inicia session aquí</a></p>
@@ -166,32 +170,44 @@
 <section class="container my-5">
     <div class="row">
         <!-- Información del usuario -->
-        <div class="col-md-4 text-center">
-            <?php if(!empty($_SESSION["user"]->getImg_profile())){?>
-                <img src="<?=url?>img/<?=$_SESSION["user"]->getImg_profile()?>" alt="Foto de <?=$_SESSION["user"]->getName()?>" class="rounded-circle mb-3" width="150" height="150">
-            <?php } else{?>
-                <div style="width: 150px; height: 150px; background: #000000; color: #fff; display: flex; align-items: center; justify-content: center; font-size: 48px; font-family: Arial, sans-serif;" class="rounded-circle mb-3"><?= mb_strtoupper(mb_substr($_SESSION["user"]->getName(), 0, 1), 'UTF-8') ?></div>
-            <?php }?>
-            
-            <h3><?=$_SESSION["user"]->getName()?></h3>
-            <p class="text-muted">Viajero</p>
-            <p><strong>1</strong> evaluación | <strong>1</strong> año en Airbnb</p>
+        <div class="col-md-3">
+            <div class="row shadow-lg rounded-4 p-4 mx-0">
+                <div class="col-md-6 text-center d-flex flex-column align-items-center">
+                    <?php if (!empty($_SESSION["user"]->getImg_profile())) { ?>
+                        <img src="<?= url ?>img/<?= $_SESSION["user"]->getImg_profile() ?>" alt="Foto de <?= $_SESSION["user"]->getName() ?>" class="rounded-circle mb-3" width="100" height="100">
+                    <?php } else { ?>
+                        <div style="width: 100px; height: 100px; background: #000000; color: #fff; display: flex; align-items: center; justify-content: center; font-size: 48px; font-family: Arial, sans-serif;" class="rounded-circle mb-3"><?= mb_strtoupper(mb_substr($_SESSION["user"]->getName(), 0, 1), 'UTF-8') ?></div>
+                    <?php } ?>
+
+                    <h3><strong><?= $_SESSION["user"]->getName() ?></strong></h3>
+                </div>
+                <div class="col-md-6 d-flex flex-column justify-content-center">
+                    <div>
+                        <p class="m-0 h4"><strong>1</strong></p>
+                        <p class="m-0 small"> evaluación</p>
+                    </div>
+                    <div class="border-top border-top-1 pt-3">
+                        <p class="m-0 h4"><strong>1</strong></p>
+                        <p class="m-0 small"> año en Airbnb</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card rounded-4 p-4 mt-4">
+                <h5>Información confirmada de <?= $_SESSION["user"]->getName() ?></h5>
+                <ul class="list-unstyled">
+                    <li><i class="bi bi-check-lg"></i> Identidad</li>
+                    <li><i class="bi bi-check-lg"></i> Dirección de correo electrónico</li>
+                    <li><i class="bi bi-check-lg"></i> Número de teléfono</li>
+                </ul>
+                <a href="#" class="text-decoration-none">Más información sobre la verificación de la identidad</a>
+            </div>
         </div>
         <!-- Información confirmada -->
-        <div class="col-md-4">
-            <h5>Información confirmada de <?=$_SESSION["user"]->getName()?></h5>
-            <ul class="list-unstyled">
-                <li>✔ Identidad</li>
-                <li>✔ Dirección de correo electrónico</li>
-                <li>✔ Número de teléfono</li>
-            </ul>
-            <a href="#" class="text-decoration-none">Más información sobre la verificación de la identidad</a>
-        </div>
         <!-- Opiniones -->
-        <div class="col-md-4">
+        <div class="col-md-9">
             <h5>Opiniones de los anfitriones sobre Alex</h5>
             <div class="border rounded p-3">
-                <p>“Encantados de recibirlos, han sido unos huéspedes excelentes.”</p>
+                <p>“Han sido unos clientes excelentes, al entregar el pedido me ofrecieron propina.”</p>
                 <small class="text-muted">- Javier, septiembre de 2024</small>
             </div>
             <a href="#" class="text-decoration-none d-block mt-3">Mostrar la evaluación</a>
